848c6bc0dc1d2dfb3f599948f0ed71a214992bee
James Davies <jaymo107@gmail.com>
2026-06-29 10:17:54 +0100

    [NOTICKET] Use $signature property on the eval test command
    The Signature attribute is not read by the lowest supported illuminate/
    console, breaking the prefer-lowest CI matrix. The property form works on
    all supported versions.

--- tests/Support/Eval/StubEvalCommand.php
+++ tests/Support/Eval/StubEvalCommand.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace AgentSoftware\LaravelAiCompanion\Tests\Support\Eval;
 
 use AgentSoftware\LaravelAiCompanion\Eval\Commands\RunEvalCommand;
-use Illuminate\Console\Attributes\Signature;
 
-#[Signature('stub:eval {target?} {--dataset=} {--out=} {--provider=} {--model=} {--tag=} {--limit=} {--trials=1}')]
-class StubEvalCommand extends RunEvalCommand {}
+class StubEvalCommand extends RunEvalCommand
+{
+    /** @var string */
+    protected $signature = 'stub:eval {target?} {--dataset=} {--out=} {--provider=} {--model=} {--tag=} {--limit=} {--trials=1}';
+}
