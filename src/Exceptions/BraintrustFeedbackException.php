08f7cb06b2fa225fc325a50eb063f3335c8c8e25
elliot-putt-spectre <elliot.putt@agentsoftware.net>
2026-07-10 10:24:46 +0100

    feat: add BraintrustFeedbackException

---
+++ src/Exceptions/BraintrustFeedbackException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AgentSoftware\LaravelAiCompanion\Exceptions;
+
+use RuntimeException;
+
+class BraintrustFeedbackException extends RuntimeException
+{
+}
